7251fa48912ce41b5a4e5a3adb6331b7348315b3
AlexanderMoon04 <alexitrix@gmail.com>
2026-07-01 22:24:35 -0600

    React Native Chart Kit added

--- apps/mobile/src/app/(app)/consumptions.tsx
+++ apps/mobile/src/app/(app)/consumptions.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {  Text } from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context"
+import {SafeAreaView} from "react-native-safe-area-context";
+import { ConsumptionsChart } from "../../components/Consumptions";
 
 export default function ConsumptionsScreen() {
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <Text>Consumptions</Text>
+      <ConsumptionsChart />
     </SafeAreaView>
   );
 }
---
+++ apps/mobile/src/components/Consumptions.tsx
@@ -0,0 +1,22 @@
+import { LineChart } from "react-native-chart-kit/v2";
+
+const data = [
+  { month: "Jan", revenue: 52 },
+  { month: "Feb", revenue: 86 },
+  { month: "Mar", revenue: 58 },
+  { month: "Apr", revenue: 134 },
+  { month: "May", revenue: 95 },
+  { month: "Jun", revenue: 176 }
+];
+
+export function ConsumptionsChart() {
+  return (
+    <LineChart
+      data={data}
+      xKey="month"
+      yKey="revenue"
+      width={410}
+      height={240}
+    />
+  );
+}
